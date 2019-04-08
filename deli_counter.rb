b58f189cbc853c070d917f865ded097e00e9d6d5
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-08 18:39:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+katz_deli = []
+
+def line (array)
+  if (array.length == 0)
+    puts "The line is currently empty."
+  else
+    counter = 0 
+    string = "The line is currently: "
+    array.each do
+      string += "#{counter+1}. #{array[counter]}"
+  end
+  puts string
+  end
+end
+
+
+def take_a_number(array, name)
+  array.push(name)
+  
+end
+
+
+def now_serving (array)
+  if (array.length == 0)
+    puts "There is nobody waiting to be served!"
+  end
+  
+  if (array.length > 0)
+  now_serve = array.shift
+  puts "Currently serving #{now_serve}."
+  end
+  return array
+end
